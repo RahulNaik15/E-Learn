ce264a118f5dd09fd92bbfff27b01689213576a9
RahulNaik15 <rahulnaik039.com>
2025-03-04 23:24:35 +0530

    added new code

--- Frontend/E-Learn/src/component/Navbar.jsx
+++ Frontend/E-Learn/src/component/Navbar.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Navbar = () => {
   return (
       <>
-          <div className='max-w-screen-2xl container mx-auto md:px-20 px-4'>
+          <div className='max-w-screen-2xl container mx-auto md:px-20 px-4 fixed top-0 left-0 right-0'>
               <div className="navbar bg-base-100 shadow-sm">
                 <div className="navbar-start">
                     <div className="dropdown">
